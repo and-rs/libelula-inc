59787e8d6a88c4eaae160545ab68ebc2665e3bc6
and-rs <deserd@protonmail.com>
2025-07-17 02:54:03 -0500

    feat: we have browser live reloading!

--- app/config.py
+++ app/config.py
@@ -5,11 +5,6 @@ from dotenv import load_dotenv
 is_dotenv_loaded = load_dotenv()
 print(f"Is dotenv loaded? {is_dotenv_loaded}")
 
-# example
-GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY", "")
-if not GEMINI_API_KEY:
-    raise ValueError(
-        "GEMINI_API_KEY not found in environment variables or .env file. Please ensure it's set."
-    )
-else:
-    print(f"Loaded api key ending with {GEMINI_API_KEY[:5]}")
+DEBUG = os.getenv("DEBUG", False)
+if not DEBUG:
+    print("Not in debug mode")
--- app/main.py
+++ app/main.py
@@ -1,22 +1,37 @@
-from fastapi import FastAPI, Request
+import arel
+from fastapi import FastAPI, Request, WebSocket
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from app.config import DEBUG
 
 app = FastAPI(title="AI Categorizer")
-
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
+
+if DEBUG:
+    hot_reload = arel.HotReload(paths=[arel.Path("app/templates")])
+
+    async def hot_reload_wrapper(websocket: WebSocket):
+        await hot_reload(websocket.scope, websocket.receive, websocket.send)
+
+    app.add_websocket_route(
+        "/hot-reload", route=hot_reload_wrapper, name="hot-reload"
+    )
+    app.add_event_handler("startup", hot_reload.startup)
+    app.add_event_handler("shutdown", hot_reload.shutdown)
+    templates.env.globals["DEBUG"] = True
+    templates.env.globals["hot_reload"] = hot_reload
 
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+async def home_section(request: Request):
+    return templates.TemplateResponse("home/home.html", {"request": request})
 
 
 @app.get("/about", response_class=HTMLResponse)
 async def about_section(request: Request):
-    return templates.TemplateResponse("about/index.html", {"request": request})
+    return templates.TemplateResponse("about/about.html", {"request": request})
 
 
 @app.get("/json", response_class=JSONResponse)
